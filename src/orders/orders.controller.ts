409cde322626058dd3a553691afe0f52b669334e
Lio10jr <kevinzambrano593@gmail.com>
2025-09-19 15:05:22 -0500

    feat(order): Añadir userId al modelo Order e implementar el método findOneByUser para la recuperación de pedidos específicos del usuario.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,7 +2,7 @@ import { Controller, ParseUUIDPipe } from '@nestjs/common';
 import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto, OrderPaginationDto, PaymentSucceededDto } from './dto';
-import { ICreateOrder } from './interfaces';
+import { PaginationDto } from 'src/common';
 
 @Controller()
 export class OrdersController {
@@ -10,13 +10,13 @@ export class OrdersController {
     private readonly ordersService: OrdersService,
   ) {}
 
+  //* Crear una orden de pago
   @MessagePattern('createOrder')
   async create(@Payload() createOrderDto: CreateOrderDto) {
-    const order: ICreateOrder  = await this.ordersService.createOrder(createOrderDto);
-    const paymentSession = await this.ordersService.paymentSession(order);
-    return paymentSession;
+    return this.ordersService.createOrder(createOrderDto);
   }
 
+  //* Obtener todas las ordenes de pago
   @MessagePattern('findAllOrders')
   findAll(
     @Payload() orderPagDto: OrderPaginationDto
@@ -24,11 +24,22 @@ export class OrdersController {
     return this.ordersService.findAll( orderPagDto );
   }
 
+  //* Obtener una orden de pago por ID
   @MessagePattern('findOneOrder')
   findOne(@Payload('id', ParseUUIDPipe) id: string) {
     return this.ordersService.findOne(id);
   }
+  
+  //* Obtener las ordenes de pago de un usuario
+  @MessagePattern('findOneByUser')
+  findOneByUser(
+    @Payload('userId', ParseUUIDPipe) userId: string,
+    @Payload('pagination') pagination: PaginationDto,
+  ) {
+    return this.ordersService.findOneByUser(userId, pagination);
+  }
 
+  //* Evento de escucha de una pago exitoso
   @EventPattern('payment.succeeded')
   paidOrder(@Payload() paySucceeded: PaymentSucceededDto) {
     this.ordersService.paidOrder(paySucceeded)
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -6,8 +6,8 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto, PaymentSucceededDto } from './dto';
 import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
-import { CardEntity, EventEntity } from 'src/common/entities';
-import { ICreateOrder } from './interfaces';
+import { CardEntity } from 'src/common/entities';
+import { PaginationDto } from 'src/common';
 
 interface CardWithPrice {
   price: number;
@@ -30,18 +30,26 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   ) { super() }
 
   async createOrder(createOrderDto: CreateOrderDto) {
+    const compensations = [];
+    const { quantity, buyer } = createOrderDto;
+
     try {
-      const { quantity } = createOrderDto;
+      // Creo las tablas de bingo
       const { price, cards, eventName } = await firstValueFrom<CardWithPrice>(
         this.client.send('createCard', createOrderDto)
       );
+      const idsCards = cards.map(card => card.id);
+      compensations.push(() => this.client.send('removeCards', idsCards));
+
       const totalAmount = price * quantity;
       const totalItems = quantity;
 
+      // Creo la orden de pago
       const order = await this.order.create({
         data: {
           totalAmount: totalAmount,
           totalItems: totalItems,
+          userId: buyer,
           orderItems: {
             createMany: {
               data: cards.map((orderItem) => ({
@@ -61,12 +69,38 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
             }
           }
         }
-      })
-      return { eventName: eventName, ...order };
+      });
+      compensations.push(() => this.order.delete({where: { id: order.id }}));
+
+      // Creo la sesión de pago
+      const payment = {
+        orderId: order.id,
+        currency: 'usd',
+        items: order.orderItems.map( (item) => ({
+            name: eventName,
+            price: item.priceUnit,
+            quantity: item.quantity,
+          }))
+      };
+
+      const paymentSession = await firstValueFrom(
+        this.client.send('create-payment-session', payment)
+      );
+
+      return paymentSession;
     } catch (error) {
+      for (const compensation of compensations) {
+        try{
+          await compensation();
+        } catch(compError) {
+          this.logger.error('Error de compensación: ' + compError);
+        }
+      }
+
+      this.logger.error('Error al crear la orden: ' +error);
       throw new RpcException({
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: error.message
+        message: 'Error al crear la orden, no fue posible crear la orden de pago'
       })
     }
   }
@@ -111,6 +145,47 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     };
   }
 
+  async findOneByUser(userId: string, pagination: PaginationDto) {
+    try {
+      const { limit, page } = pagination;
+      const totalPages = await this.order.count({
+        where: { userId }
+      });
+      const currentPage = page;
+      const perPage = limit;
+
+      return {
+        data: await this.order.findMany({
+          skip: (currentPage - 1) * perPage,
+          take: perPage,
+          where: {
+            userId
+          },
+          include: {
+            orderItems: {
+              select: {
+                cardId: true,
+                priceUnit: true,
+                quantity: true
+              }
+            }
+          }
+        }),
+        meta: {
+          total: totalPages,
+          page: currentPage,
+          lastPage: Math.ceil(totalPages / perPage)
+        }
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw new RpcException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error al obtener las ordenes del usuario',
+      })
+    }
+  }
+  
   async findOne(id: string) {
     try {
       const order = await this.order.findFirst({
@@ -130,56 +205,44 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
       if (!order) throw new RpcException({
         status: HttpStatus.NOT_FOUND,
-        message: `Order with id #${id} not found`
+        message: `No se encontro la orden con id ${id}`
       });
 
       return  order;
-
     } catch (error) {
       throw new RpcException({
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: error.message,
+        message: 'Error al obtener la orden',
       })
     }
-  }
-
-  async paymentSession(order: ICreateOrder) {
-    try {
-      const paymentSession = await firstValueFrom(
-        this.client.send('create-payment-session', {
-          orderId: order.id,
-          currency: "usd",
-          items: order.orderItems.map( (item) => ({
-            name: order.eventName,
-            price: item.priceUnit,
-            quantity: item.quantity,
-          }))
-        })
-      );
-      return paymentSession;
-    } catch (error) {
-      throw new RpcException({
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: `Error in comunicate with payments ms`
-      })
-    }
-
   }
 
   async paidOrder(paySecceeded: PaymentSucceededDto) {
-    await this.order.update({
-      where: { id: paySecceeded.orderId },
-      data: {
-        status: 'PAID',
-        paid: true,
-        paidAt: new Date(),
-        stripeChargeId: paySecceeded.stripePaymentId,
-        OrderReceipt: {
-          create: {
-            receiptUrl: paySecceeded.receiptUrl
+    try {
+      // Actualizo la orden
+      await this.order.update({
+        where: { id: paySecceeded.orderId },
+        data: {
+          status: 'PAID',
+          paid: true,
+          paidAt: new Date(),
+          stripeChargeId: paySecceeded.stripePaymentId,
+          OrderReceipt: {
+            create: {
+              receiptUrl: paySecceeded.receiptUrl
+            }
           }
-        }
-      },
-    })
+        },
+      });
+
+      const ids = (await this.orderItem.findMany({ where: { orderId: paySecceeded.orderId }})).map(orderItem => orderItem.cardId);
+      // Habilito la card
+      await firstValueFrom(
+        this.client.send('updateAvailableManyCard', {ids})
+      );
+
+    } catch(error) {
+      this.logger.error('Error al actualizar la orden de pago: ' + {error});
+    }
   }
 }
